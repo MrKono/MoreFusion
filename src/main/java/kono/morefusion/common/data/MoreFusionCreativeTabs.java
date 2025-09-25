9a30e4f47ab3609e336a5878d268dcae4eb8ef0f
ko_no <90126004+MrKono@users.noreply.github.com>
2025-09-25 22:59:56 +0900

    sppppppppppppotless

--- src/main/java/kono/morefusion/common/data/MoreFusionCreativeTabs.java
+++ src/main/java/kono/morefusion/common/data/MoreFusionCreativeTabs.java
@@ -1,19 +1,21 @@
 package kono.morefusion.common.data;
 
+import net.minecraft.world.item.CreativeModeTab;
+
 import com.gregtechceu.gtceu.common.data.GTBlocks;
 import com.gregtechceu.gtceu.common.data.GTCreativeModeTabs;
-import com.gregtechceu.gtceu.common.data.machines.GTMultiMachines;
-import com.tterrag.registrate.util.entry.RegistryEntry;
-import net.minecraft.world.item.CreativeModeTab;
 
 import static kono.morefusion.api.MoreFusionValues.modId;
 import static kono.morefusion.common.data.MoreFusionRegistration.REGISTRATE;
 
+import com.tterrag.registrate.util.entry.RegistryEntry;
+
 public class MoreFusionCreativeTabs {
+
     public static RegistryEntry<CreativeModeTab> MOREFUSION = REGISTRATE.defaultCreativeTab("item",
-                    builder -> builder.displayItems(new GTCreativeModeTabs.RegistrateDisplayItemsGenerator("item", REGISTRATE))
-                            .icon(GTBlocks.FUSION_CASING_MK3::asStack)
-                            .title(REGISTRATE.addLang("itemGroup", modId("item"), "MoreFusion"))
-                            .build())
+            builder -> builder.displayItems(new GTCreativeModeTabs.RegistrateDisplayItemsGenerator("item", REGISTRATE))
+                    .icon(GTBlocks.FUSION_CASING_MK3::asStack)
+                    .title(REGISTRATE.addLang("itemGroup", modId("item"), "MoreFusion"))
+                    .build())
             .register();
 }
